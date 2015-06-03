a4e0ca88df9075de51f23bc08bc77388dad683c8
Chiara-yen <chiyen.chiara@gmail.com>
2015-06-03 16:36:28 +0800

    transform ex-03 to koa-static server

--- ex-03-state-props/webpack.js
+++ ex-03-state-props/webpack.js
@@ -1,10 +1,10 @@
 module.exports = {
   entry: {
-    app: './app.jsx'
+    app: './public/app.jsx'
   },
   output: {
     // __dirname is the path of webpack.js
-    path: __dirname,
+    path: __dirname + '/public',
     filename: '[name].js'
   },
   module: {
